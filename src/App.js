55b4b024965e525d58a9ec0ba2bf838424af90de
unknown <chubocon2015@gmail.com>
2020-11-29 22:50:58 +0700

    'BaiTapThucHanhLayout'

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import DemoFunctionComponent from './Components/DemoFunctionComponent';
+import DemoReactClassComponent from './Components/DemoReactClassComponent';
+import BaiTapLayout from './Components/BaiTapLayout/BaiTapLayout';
+import BaiTapThucHanhLayout from './BaiTapThucHanhLayout/BaiTapThucHanhLayout';
+
+function App() {
+  return (
+    <div className="App">
+      <BaiTapThucHanhLayout/>
+      
+    </div>
+  );
+}
+
+export default App;
